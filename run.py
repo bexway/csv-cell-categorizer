c4d4e06e9cfd38032a0705721b2479b594c76c8d
Bex Way <rebeccaway2016@u.northwestern.edu>
2017-09-08 23:50:27 -0500

    master - added placeholder database

--- run.py
+++ run.py
@@ -3,6 +3,14 @@ import Tkinter as tk
 import tkFileDialog
 
 def main():
+    patterns = ["[tT]est ?1", "[Pp]atterns?", "([Tt]hree)|3"]
+
+    databaseFile = open("database.csv", 'rb')
+    databaseReader = csv.DictReader(databaseFile)
+
+    for row in databaseReader:
+        #placeholder to make sure it's reading right
+        print row
 
 if __name__ == "__main__":
     main()
