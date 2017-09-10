e20d10284dd4c1c04bc4cbbcc7ee71785158da1a
Bex Way <rebeccaway2016@u.northwestern.edu>
2017-09-10 00:46:03 -0500

    master - added output writer

--- run.py
+++ run.py
@@ -3,14 +3,25 @@ import Tkinter as tk
 import tkFileDialog
 
 def main():
+    # I'll use regexes to match the favorite subject to the
     patterns = ["[tT]est ?1", "[Pp]atterns?", "([Tt]hree)|3"]
 
-    databaseFile = open("database.csv", 'rb')
-    databaseReader = csv.DictReader(databaseFile)
+    inputFile = open("database.csv", 'rb')
+    inputReader = csv.DictReader(inputFile)
 
-    for row in databaseReader:
+    inputHeaders = inputReader.fieldnames[:]
+    outputHeaders = inputHeaders + ["Fav Subject 1", "Fav Subject 2", "Fav Subject 3"]
+
+    outputFile = open("output.csv", 'wb')
+    outputWriter = csv.DictWriter(outputFile, outputHeaders)
+    outputWriter.writeheader()
+    
+    for row in inputReader:
         #placeholder to make sure it's reading right
         print row
+
+    inputFile.close()
+    outputFile.close()
 
 if __name__ == "__main__":
     main()
