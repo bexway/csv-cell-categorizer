29aa0cc558509cb9e1acc3a4ea0d48044a2acd61
Bex Way <rebeccaway2016@u.northwestern.edu>
2017-09-14 11:04:52 -0500

    master - added initial file writing for output

--- run.py
+++ run.py
@@ -11,37 +11,48 @@ def main():
     inputReader = csv.DictReader(inputFile)
 
     inputHeaders = inputReader.fieldnames[:]
-    outputHeaders = inputHeaders + ["Fav Subject 1", "Fav Subject 2", "Fav Subject 3", "All Fav Subjects"]
+    outputHeaderFields = ["Fav Subject 1", "Fav Subject 2", "Fav Subject 3", "All Fav Subjects"]
+    outputHeaders = inputHeaders + outputHeaderFields
 
     outputFile = open("output.csv", 'wb')
     outputWriter = csv.DictWriter(outputFile, outputHeaders)
     outputWriter.writeheader()
-    
+
     for row in inputReader:
         #get the input and create a list to hold the output
         inputSubjects =  row["Favorite Subject(s)"]
-        outputSubjects = []
+        outputMatches = []
         #check each pattern for matches
         #TODO: append the consistent category, not the pattern
         for pattern in patternList:
             re = regex.compile(pattern)
             if re.search(inputSubjects):
-                outputSubjects.append(pattern)
+                outputMatches.append(pattern)
 
-        box = DialogBox(inputSubjects, outputSubjects)
-
+        box = DialogBox(inputSubjects, outputMatches)
 
         print inputSubjects
-        print outputSubjects
+        print outputMatches
         print box.output
 
-    # box = DialogBox()
+        parsedOutputs = box.output
+
+        outputWritingDict = {}
+        for key in outputHeaders:
+            if key in outputHeaderFields:
+                if len(parsedOutputs) != 0:
+                    outputWritingDict[key] = parsedOutputs.pop(0)
+            else:
+                outputWritingDict[key] = row[key]
+        outputWriter.writerow(outputWritingDict)
+
+
 
     inputFile.close()
     outputFile.close()
 
 class DialogBox(tk.Tk):
-    def __init__(self, inputSubjects, outputSubjects):
+    def __init__(self, inputSubjects, outputMatches):
         """Open a dialog box for submitting majors"""
         tk.Tk.__init__(self)
         self.label = tk.Label(self, text="\nPlease ensure the boxes contain the correct subjects. See the input for what the student submitted. When you're done, press 'Submit'.\n", wraplength=600)
@@ -51,7 +62,7 @@ class DialogBox(tk.Tk):
         self.subjectEntries = []
         # Add an input box with default text for each subject that was matched. If less than 5 subjects match, add extra empty boxes to fill in.
         # subjectEntries tracks the entry boxes for later retrieval.
-        for m in outputSubjects:
+        for m in outputMatches:
             self.entry = tk.Entry(self, width=50)
             self.entry.insert(0, m)
             self.entry.pack()
@@ -60,7 +71,7 @@ class DialogBox(tk.Tk):
             self.entry = tk.Entry(self, width=50)
             self.entry.pack()
             self.subjectEntries.append(self.entry)
-        #TODO: see if I can have a button to pack in? Can I add to tkinter boxes dynamically?
+        #TODO: see if I can have a button to pack in an additional input box? Can I add to tkinter boxes dynamically?
         
 
         self.button = tk.Button(self, text="Submit", command=self.submit)
